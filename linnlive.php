acbe1adab18dde9bab8a1cb07d08d8d2555a7df7
John Mitchell <jfm@minioak.com>
2012-08-01 19:57:00 +0100

    Adding basic functions

--- linnlive.php
+++ linnlive.php
@@ -13,5 +13,33 @@
 
 public class LinnLive 
 {
-	
+	protected $settings = array();
+		
+	protected function default_settings()
+	{
+		return array(
+			'api_key' = ''
+		);
+	}
+
+	public function __construct($settings = false)
+	{
+		parent::_construct();
+		if (is_array($settings))
+		{
+			$this->initialize($settings);
+		}
+	}
+		
+	public function initialize($settings)
+    {
+	    foreach ($this->default_settings() as $key => $default)
+	    {
+            if (isset($settings[$key]))
+            {
+                // boolean settings must remain booleans
+                $this->settings[$key] = is_bool($default) ? (bool)$settings[$key] : $settings[$key];
+            }
+	    }
+    }
 }
