5d567d7f0a6bb5a53af89582597c8b4cd56c8586
John Mitchell <jfm@minioak.com>
2012-08-01 23:55:44 +0100

    Removing api key

--- tests/get_stock.php
+++ tests/get_stock.php
@@ -3,5 +3,5 @@
 require_once(dirname(__FILE__).'/../linnlive.php');
 
 $linn = new LinnLive();
-$linn->initialize(array('api_key' => '35B6E0463971413FB172B4EDA993DCF3'));
+$linn->initialize(array('api_key' => ''));
 var_dump($linn->get_stock());
