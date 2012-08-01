ba94b4357b6d2a854f7175e0b60fd0ddd288232a
John Mitchell <jfm@minioak.com>
2012-08-01 17:11:16 +0100

    Skeleton class for Linnlive wrapper class

---
+++ linnlive.php
@@ -0,0 +1,17 @@
+<?php
+
+/* ***************************************************************
+// LinnLive
+// Copyright (c) 2012 John Mitchell - http://www.minioak.com
+// 
+// Version: 1.0.0 (1/8/2012)
+// 
+// Dual licensed under the MIT and GPL licenses:
+// http://www.opensource.org/licenses/mit-license.php
+// http://www.gnu.org/licenses/gpl.html
+// ***************************************************************/
+
+public class LinnLive 
+{
+	
+}
