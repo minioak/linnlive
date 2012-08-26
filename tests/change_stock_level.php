03d8c15709eb64ad05b4695649e1c9cae3c31384
John Mitchell <jfm@minioak.com>
2012-08-27 00:20:29 +0100

    Except that wasn't an api key. Need to get some sleep.

--- tests/change_stock_level.php
+++ tests/change_stock_level.php
@@ -6,7 +6,7 @@ $linn = new LinnLive_stock();
 $linn->initialize(array('api_key' => ''));
 
 $update = $linn->update_stock_level(array(
-	'stock_id' => '', // Stock item id as defined by LinnLive (i.e not the SKU - but you should be able to map it from the inventory dump)
+	'stock_id' => '094ecf55-1fe8-4e37-8289-f4772b0e96e9', // Stock item id as defined by LinnLive (i.e not the SKU - but you should be able to map it from the inventory dump)
 	'location' => 'Default', // Location as defined by LinnLive,
 	'level' => 10 // Number of items in stock
 ));
