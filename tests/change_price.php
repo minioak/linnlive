69eb93d8d7cf532565c4374713c638ecec5814b6
John Mitchell <jfm@minioak.com>
2012-08-27 08:26:33 +0100

    #6 Adding change of price support. Getting error from web service at the moment though

---
+++ tests/change_price.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(dirname(__FILE__).'/../stock.php');
+
+$linn = new LinnLive_stock();
+$linn->initialize(array('api_key' => '8E27B7A94223428C9E4F5077B7FEC4BF'));
+
+$update = $linn->update(array(
+	'stock_id' => '094ecf55-1fe8-4e37-8289-f4772b0e96e9', // Stock item id as defined by LinnLive (i.e not the SKU - but you should be able to map it from the inventory dump)
+	'price' => '10.00'
+));
+
+var_dump($update);
